7f32c79a6d9a874588e708687d5314c0cc8e4db4
Kareem <hshot9930@gmail.com>
2015-12-05 19:09:51 -0400

    Update EventListener.php

--- src/cmdsnooper/EventListener.php
+++ src/cmdsnooper/EventListener.php
@@ -1,1 +1,38 @@
 <?php
+
+namespace cmdsnooper;
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerCommandPreprocessEvent;
+use cmdsnooper\CmdSnooper;
+
+class EventListener implements Listener {
+	public $plugin;
+	
+	public function __construct(CmdSnooper $plugin) {
+		$this->plugin = $plugin;
+	}
+
+	public function getPlugin() {
+		return $this->plugin;
+	}
+	
+	public function onPlayerCmd(PlayerCommandPreprocessEvent $event) {
+		$sender = $event->getPlayer();
+		$msg = $event->getMessage();
+		
+		if($this->getPlugin()->cfg->get("Console.Logger") == "true") {
+			if($msg[0] == "/") {
+				$this->getPlugin()->getLogger()->info($sender->getName() . "> " . $msg);
+				}
+			}
+			
+			if(!empty($this->getPlugin()->snoopers)) {
+				foreach($this->getPlugin()->snoopers as $snooper) {
+					 if($msg[0] == "/") {
+				   	$snooper->sendMessage("§7Snoop> " . $sender->getName() . "> " . $msg);
+					}
+	     }		
+     }
+   	}
+	}
