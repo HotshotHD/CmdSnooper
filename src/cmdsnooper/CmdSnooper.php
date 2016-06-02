23fa0a0a3c9a5c2e4e161105324f934bde71dabc
Kareem <hshot9930@gmail.com>
2016-06-02 18:40:50 -0400

    Code cleanup

--- src/cmdsnooper/CmdSnooper.php
+++ src/cmdsnooper/CmdSnooper.php
@@ -15,19 +15,19 @@ class CmdSnooper extends PluginBase {
 		@mkdir($this->getDataFolder());
 		$this->getLogger()->info("Enabled! Ready to snoop >:D");
 		$this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
-	  $this->cfg = new Config($this->getDataFolder() . "config.yml", Config::YAML, array(
-	  "Console.Logger" => "true",
-  	));
+		$this->cfg = new Config($this->getDataFolder() . "config.yml", Config::YAML, array(
+	  	"Console.Logger" => "true",
+  		));
 	}
 	
 	 public function onCommand(CommandSender $sender, Command $cmd, $label, array $args) {			
-		if(strtolower($cmd->getName()) == "snoop")	{
-		 if($sender instanceof Player) {
-			if($sender->hasPermission("snoop.command")) {
-				if(!isset($this->snoopers[$sender->getName()])) {
-					$sender->sendMessage("§7Snoop> §fYou have entered snoop mode");
-					$this->snoopers[$sender->getName()] = $sender;
-					return true;
+		if(strtolower($cmd->getName()) == "snoop") {
+		 	if($sender instanceof Player) {
+				if($sender->hasPermission("snoop.command")) {
+					if(!isset($this->snoopers[$sender->getName()])) {
+						$sender->sendMessage("§7Snoop> §fYou have entered snoop mode");
+						$this->snoopers[$sender->getName()] = $sender;
+						return true;
 					} else {
 						$sender->sendMessage("§7Snoop> §fYou have left snoop mode");
 						unset($this->snoopers[$sender->getName()]);
