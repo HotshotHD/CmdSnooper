c4716a4a681ef438718f190d8cd9b1eb0b7ed540
jazzwhistle <jdcraftmcpe@gmail.com>
2016-06-03 18:36:47 +0200

    Added config to hide /tell in game or console Removed unnecessary log messages changed logic for password commands

--- src/cmdsnooper/EventListener.php
+++ src/cmdsnooper/EventListener.php
@@ -7,42 +7,47 @@ use pocketmine\event\player\PlayerCommandPreprocessEvent;
 use cmdsnooper\CmdSnooper;
 
 class EventListener implements Listener {
-	public $plugin;
-	
-	public function __construct(CmdSnooper $plugin) {
-		$this->plugin = $plugin;
-	}
 
-	public function getPlugin() {
-		return $this->plugin;
-	}
-	
-	public function onPlayerCmd(PlayerCommandPreprocessEvent $event) {
-		$sender = $event->getPlayer();
-		$msg = $event->getMessage();
-		
-		if($this->getPlugin()->cfg->get("Console.Logger") == "true") {
-			if($msg[0] == "/") {
-				if(stripos($msg, "login") || stripos($msg, "log") || stripos($msg, "reg") || stripos($msg, "register") || stripos($msg, "chpwd")) {
-					$this->getPlugin()->getLogger()->info($sender->getName() . "> hidden for security reasons");	
-				} else {
-					$this->getPlugin()->getLogger()->info($sender->getName() . "> " . $msg);
-				}
-				
-			}
-		}
-			
-			if(!empty($this->getPlugin()->snoopers)) {
-				foreach($this->getPlugin()->snoopers as $snooper) {
-					 if($msg[0] == "/") {
-						if(stripos($msg, "login") || stripos($msg, "log") || stripos($msg, "reg") || stripos($msg, "register") || stripos($msg, "chpwd")) {
-							$snooper->sendMessage($sender->getName() . "> hidden for security reasons");	
-						} else {
-							$snooper->sendMessage($sender->getName() . "> " . $msg);
-						}
-						
-					}
-	     			}		
-     			}
-   		}
-	}
+    public $plugin;
+
+    public function __construct(CmdSnooper $plugin) {
+        $this->plugin = $plugin;
+    }
+
+    public function getPlugin() {
+        return $this->plugin;
+    }
+
+    public function onPlayerCmd(PlayerCommandPreprocessEvent $event) {
+        $sender = $event->getPlayer();
+        $msg = $event->getMessage();
+
+        if ($this->getPlugin()->cfg->get("Console.Logger") == "true") {
+            if ($msg[0] == "/") {
+
+                $hasTell = stripos($msg, "tell") && $this->getPlugin()->cfg->get("Console.HideTell");
+
+//                            if($this->getPlugin()->cfg->get("Console.HideTell") == "true") {
+//                            $hasTell = true;
+//                            }
+                if (!($hasTell || stripos($msg, "log") || stripos($msg, "reg") || stripos($msg, "pwd"))) {
+                    $this->getPlugin()->getLogger()->info($sender->getName() . " > " . $msg);
+                }
+            }
+        }
+
+        if (!empty($this->getPlugin()->snoopers)) {
+
+            $hasTell = stripos($msg, "tell") && $this->getPlugin()->cfg->get("Game.HideTell");
+
+            foreach ($this->getPlugin()->snoopers as $snooper) {
+                if ($msg[0] == "/") {
+                    if (!($hasTell || stripos($msg, "log") || stripos($msg, "reg") || stripos($msg, "chpwd"))) {
+                        $snooper->sendMessage($sender->getName() . " > " . $msg);
+                    }
+                }
+            }
+        }
+    }
+
+}
